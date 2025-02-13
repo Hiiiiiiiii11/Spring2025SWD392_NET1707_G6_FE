0cf30c7b133fda5625800d9bb32f254b49ab3432
QuocHuy <huyhqse173627@fpt.edu.vn>
2025-02-13 18:33:56 +0700

    add signup router

--- src/routers/router.js
+++ src/routers/router.js
@@ -9,6 +9,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Login />} />
             </Routes>
         </Router>
     );
