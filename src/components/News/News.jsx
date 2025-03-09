100cf2d382a55ebaaa3b9e4cebddaf1d2e45c902
QuocHuy <huyhqse173627@fpt.edu.vn>
2025-03-09 13:36:20 +0700

    fix conflict

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-import { List, Typography, Card, Button } from "antd";
-<<<<<<< HEAD
-=======
 import "./News.css";
->>>>>>> parent of d0bc59a (css new blog)
-=======
-import { Typography, Card, Button } from "antd";
+import { Typography, Card, Button, List } from "antd";
 import "./News.css";
->>>>>>> 971e8fca551bd2097b5175e8930e80d68cc8512d
 
 const { Title, Text } = Typography;
 
@@ -33,13 +26,11 @@ const News = () => {
       <Title level={2} className="news-title">
         Latest Skincare News
       </Title>
-<<<<<<< HEAD
       <List
         grid={{ gutter: 16, column: 2 }}
         dataSource={newsItems}
         renderItem={(item) => (
           <List.Item>
-<<<<<<< HEAD
             <Card
               hoverable
               cover={
@@ -54,15 +45,7 @@ const News = () => {
               <Title level={4}>{item.title}</Title>
               <Text>{item.content}</Text>
               <div style={{ marginTop: 10, textAlign: "right" }}>
-=======
-            <Card hoverable className="news-card">
-              <img alt={item.title} src={item.image} className="news-image" />
-              <Title level={4} className="news-card-title">
-                {item.title}
-              </Title>
-              <Text className="news-card-content">{item.content}</Text>
-              <div className="news-card-button">
->>>>>>> parent of d0bc59a (css new blog)
+
                 <Button type="primary">Read More</Button>
               </div>
             </Card>
@@ -84,7 +67,6 @@ const News = () => {
           </Card>
         ))}
       </div>
->>>>>>> 971e8fca551bd2097b5175e8930e80d68cc8512d
     </div>
   );
 };
